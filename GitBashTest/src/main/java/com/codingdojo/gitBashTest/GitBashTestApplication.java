a65aa40c3a275556b57ecc1f0b3d0d70fdc9b733
Alexander Justiniano <alexan.justiniano001@mymdc.net>
2024-06-29 17:44:34 -0400

    First Commit

---
+++ GitBashTest/src/main/java/com/codingdojo/gitBashTest/GitBashTestApplication.java
@@ -0,0 +1,13 @@
+package com.codingdojo.gitBashTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitBashTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitBashTestApplication.class, args);
+	}
+
+}
---
+++ GitBashTest/src/test/java/com/codingdojo/gitBashTest/GitBashTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingdojo.gitBashTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitBashTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
